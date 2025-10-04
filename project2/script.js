fce774dca7cb1cf8898797b791d47722208ac507
Bryan-RB44 <bryan.briones44@gmail.com>
2025-10-04 03:08:28 -0400

    First Commit/WIP 1: Got the scrolling effect to work, added some comments, finished foreground skyline art.

---
+++ project2/script.js
@@ -0,0 +1,27 @@
+window.onload = ()=>{
+    console.log("Hello! This Works!");
+
+    // First skyline is found and selected
+    const cityFront = document.querySelector(".cityFront");
+    // Keeps adding the foreground skyline to the image track in the second div (class="cityFront")
+    cityFront.innerHTML += cityFront.innerHTML;
+
+    const cityBack = document.querySelector(".cityBack");
+    cityBack.innerHTML += cityBack.innerHTML;
+
+    // This section uses GSAP to animate the scrolling effect for all of the image tracks
+    // FORGROUND SKYLINE
+    gsap.to(cityFront, {
+        x: "-50%",
+        duration: 120,
+        repeat: -1,
+        ease: "none"
+    });
+    //BACKGROUND SKYLINE
+    gsap.to(cityBack , {
+        x: "-50%",
+        duration: 180,
+        repeat: -1,
+        ease: "none"
+    });
+};
