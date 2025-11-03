3c951748eaeb1cbbd830c78f41b1e7fa414bb4d7
Bryan-RB44 <bryan.briones44@gmail.com>
2025-11-03 16:56:00 -0500

    class demo again

--- class-demos/week_08/vue-project/src/App.vue
+++ class-demos/week_08/vue-project/src/App.vue
@@ -1,9 +1,21 @@
-<script setup></script>
+<script setup>
+  import { ref } from "vue"
+
+  const myVariable = ref('hi')
+
+  const greetings = () =>{
+    console.log("hi")
+  }
+
+  // onMounted ( ()=>{
+  //   console.log(document.getElementById('blue-paragraph'))
+  // })
+</script>
 
 <template>
   <h1>You did it!</h1>
-  <button>Hi</button>
-  <p>
+  <button @click="greetings">{{ myVariable }}</button>
+  <p id="blue-paragraph">
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
